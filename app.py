b57235a3c61d785d78fa1960289a3b0c5bd46a8a
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-05-03 21:30:06 +0000

    May 3, 2024, 3:30 PM

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    var_test_pass = "abc123"
+    cake = "blorbo"
+    moo "cow"
     return "Hello, Flask!"
