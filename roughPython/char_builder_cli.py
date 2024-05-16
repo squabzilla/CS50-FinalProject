a3c2d3b7c070f0e718bf6da4ff98d660a8f5eaff
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-05-16 17:17:57 -0600

    dsaf

---
+++ roughPython/char_builder_cli.py
@@ -0,0 +1,31 @@
+def homescreen():
+    print(\
+        "Welcome to the character creator!\n"\
+        "1. Create new character\n"\
+        "2. Load a character\n"\
+        "3. Quit")
+    #var_choice = str(input(""))
+    var_choice = input("")
+    
+    #print("You entered ", len(var_choice), "characters.")
+    #print("All the characters were numeric: ", var_choice.isnumeric())
+    
+    if len(var_choice) != 1:
+        print("error: invalid number of characters")
+        return 1
+    if var_choice.isnumeric() == False:
+        print("error: invalid input - please input a number from 1 to 3")
+        return 1
+    var_choice = int(var_choice)
+    if var_choice == 3:
+        print("Quit")
+        exit()
+    print("here")
+
+
+def main():
+    while True: homescreen()
+
+
+
+main()
