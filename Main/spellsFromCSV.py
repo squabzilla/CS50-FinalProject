01e3bfe76a506a61bf36a19c8913fc1552b46744
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-07 05:17:56 +0000

    Jun 6, 2024, 11:17 PM

--- Main/spellsFromCSV.py
+++ Main/spellsFromCSV.py
@@ -9,6 +9,6 @@ with open("spellsFromCSV.py", "r") as var_file:
 	next(var_reader)
 	#skip header row
 	for var_row in var_reader:
-		print(var_row[1])
+		print(var_row)
 		# print the second column of the CSV
 # this means that the file will be automatically closed once outside with with block
