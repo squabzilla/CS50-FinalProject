347bf6f160fd450d8870de1a7cfb209d7746cd4b
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-07 17:18:24 +0000

    Jun 7, 2024, 11:18 AM

--- Main/app.py
+++ Main/app.py
@@ -1,13 +1,27 @@
+### below: copied imported libraries from finance-problem app.py ###
 import os
-
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-#from helpers import apology, login_required, lookup, usd, usd_to_variables, month_number_and_text
+### above: copied imported libraries from finance-problem app.py ###
 
+# configure flask application
 from flask import Flask
 app = Flask(__name__)
+
+# configure session settings
+app.config["SESSION_PERMANENT"] = False
+Session(app)
+
+# session cleanup notes:
+# Run the the following command regularly with a cron job or scheduler
+# such as Heroku Scheduler to clean up expired sessions.
+# This is the recommended way to clean up expired sessions.
+# command is: flask session_cleanup
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///RPG_characters.db")
 
 @app.route("/")
 def home():
