09519ddb5262310b998761502130229cb9c61c2a
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-07 06:21:30 +0000

    Jun 7, 2024, 12:21 AM

--- Main/spellsFromCSV.py
+++ Main/spellsFromCSV.py
@@ -3,6 +3,9 @@ import csv
 from cs50 import SQL
 #import cs50
 
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
 with open("spell_list.csv", "r") as var_file:
 	# do stuff with file
 	var_reader = csv.reader(var_file)
@@ -15,3 +18,8 @@ with open("spell_list.csv", "r") as var_file:
 		if counter > 5: break
 		# print the second column of the CSV
 # this means that the file will be automatically closed once outside with with block
+
+#db.execute("UPDATE users SET cash = ? WHERE id = ?", new_client_cash, client_id)
+#DELETE FROM Customers WHERE CustomerName='Alfreds Futterkiste';
+
+db.execute("INSERT INTO list_races (race_id, race_name) VALUES (10, 'goblin')")
