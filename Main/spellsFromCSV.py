aa80dbabedcd03cc7f4d9c73a2cc46964a191a3b
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-07 06:13:13 +0000

    Jun 7, 2024, 12:13 AM

--- Main/spellsFromCSV.py
+++ Main/spellsFromCSV.py
@@ -3,12 +3,15 @@ import csv
 #from cs50 import SQL
 #import cs50
 
-with open("spellsFromCSV.py", "r") as var_file:
+with open("spell_list.csv", "r") as var_file:
 	# do stuff with file
 	var_reader = csv.reader(var_file)
 	next(var_reader)
 	#skip header row
+	counter = 0
 	for var_row in var_reader:
-		print(var_row)
+		print(var_row[1])
+		counter += 1
+		if counter > 5: break
 		# print the second column of the CSV
 # this means that the file will be automatically closed once outside with with block
