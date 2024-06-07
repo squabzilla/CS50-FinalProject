f1324b022b47a572e61ffdb25b3b921318c44d9c
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-07 17:49:17 +0000

    Jun 7, 2024, 11:49 AM

--- Main/app.py
+++ Main/app.py
@@ -1,10 +1,13 @@
-### below: copied imported libraries from finance-problem app.py ###
+#########################################################################################
+### below: copied imported libraries from finance-problem app.py 
 import os
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-### above: copied imported libraries from finance-problem app.py ###
+from helpers import login_required
+### above: copied imported libraries from finance-problem app.py 
+#########################################################################################
 
 # configure flask application
 from flask import Flask
@@ -49,8 +52,8 @@ def home():
     #return render_template("template.html")
     return render_template("index.html")
 
-########################################################################################
-### below: copied the "login" and "logout" functionality from finance-problem app.py ### 
+#########################################################################################
+### below: copied the "login" and "logout" functionality from finance-problem app.py 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
@@ -99,6 +102,6 @@ def logout():
 
     # Redirect user to login form
     return redirect("/")
-### above:  copied the "login" and "logout" functionality from finance-problem app.py ###
+### above:  copied the "login" and "logout" functionality from finance-problem app.py
 #########################################################################################
 
---
+++ Main/helpers.py
@@ -0,0 +1,19 @@
+#########################################################################################
+### below: copied login_required function from finance-problem helpers.py
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
+### above: copied login_required function from finance-problem helpers.py
+#########################################################################################
