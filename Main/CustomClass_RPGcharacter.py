045df6a425f2012d13b8da0a814fadc0dbee8c99
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-13 08:16:00 +0000

    Jun 13, 2024, 2:16 AM

--- Main/CustomClass_RPGcharacter.py
+++ Main/CustomClass_RPGcharacter.py
@@ -31,49 +31,76 @@ class rpg_character:
         # self.feature.character_id: no, each logged-in user has their own unique user_id which we can retrieve 
         # self.feature.pc_feature_id:   stored in above dictionary  ref: INTEGER
         # self.feature.list_order:      stored in above dictionary  ref: INTEGER,   FOREIGN KEY(pc_feature_id) REFERENCES list_pc_features(pc_feature_id)
-        def validate_races(race_max):
-            if self.race_id.isnumeric() == False:
-                return False
-            elif self.race_id > race_max:
-                return False
+        def numberify_race_id():
+            try:
+                int(self.race_id)
+                return True
+            except: return False
+        def numberify_class_id():
+            try:
+                int(self.class_id)
+                return True
+            except: return False
+        def numberify_background_id():
+            try:
+                int(self.background_id)
+                return True
+            except: return False
+        def numberify_spells():
+            for spell in self.spells_known:
+                try: int(self.spells_known)
+                except: return False
             return True
+
+def validate_rpgCharacter_entry(entry_value, maximum_value)
+        
+# class Person:
+  # def __init__(self, name, age):
+    # self.name = name
+    # self.age = age
+
+  # def myfunc(self):
+    # print("Hello my name is " + self.name)
+
+# p1 = Person("John", 36)
+# p1.myfunc()
 
 ##### NOTE: at some point, I want to make a function that takes an rpg_character as input and validates that all the entries are correct
 
 
 
-def valid_race_id(db):
-    var_column = "race_id"
-    var_table = "list_races"
-    #race_query = db.execute("SELECT ? FROM ?;")
-    #race_query = (db.execute("SELECT COUNT(*) FROM list_races;")).get("COUNT(*)")
-    race_query = db.execute("SELECT COUNT(*) FROM list_races;")
-    values_dict = {}
-    values_dict.update(race_query[0])
-    race_count = db.execute("SELECT COUNT(*) FROM list_races;")[0].get("COUNT(*)")
-    class_count = db.execute("SELECT COUNT(*) FROM list_pc_classes;")[0].get("COUNT(*)")
-    background_count = db.execute("SELECT COUNT(*) FROM list_backgrounds;")[0].get("COUNT(*)")
-    spells_count = db.execute("SELECT COUNT(*) FROM list_spells;")[0].get("COUNT(*)")
-    features_count = db.execute("SELECT COUNT(*) FROM list_pc_features;")[0].get("COUNT(*)")
-    # okay so db.execute returns a list of dicts, so I grab the first element of that dict, and grab the value-pair of that dict
-    # gods that is cursed, but it works lmao
-    print("Race count is: ", race_count)
-    #race_query = race_query.get("COUNT(*)")
-    #var_count = 'COUNT(*)'
-    #race_query = db.execute("SELECT ? FROM list_races", var_count)
-    # this returns a dictionary, figure out how to properly interact with dictionaries and you should be good
-    # a dictionary item on each line for each column I looked at
-    # do yourself a favour and just zero-index all your CSVs, in the like AUTO-INCREMENT format and all that
-    # that way, instead of going through all the items to check the primary key id,
-    # I can just get a count of the number of items in the database
-    race_list = []
-    print("here:", race_query[0])
-    #for var_line in race_query:
-        #print(var_line)
-        #for var_item in var_line:
-            #print(var_item)
-        #race_list.append(var_line[1])
-    return race_query
+# def valid_race_id(db):
+    # var_column = "race_id"
+    # var_table = "list_races"
+    # #race_query = db.execute("SELECT ? FROM ?;")
+    # #race_query = (db.execute("SELECT COUNT(*) FROM list_races;")).get("COUNT(*)")
+    # race_query = db.execute("SELECT COUNT(*) FROM list_races;")
+    # values_dict = {}
+    # values_dict.update(race_query[0])
+    # race_count = db.execute("SELECT COUNT(*) FROM list_races;")[0].get("COUNT(*)")
+    # class_count = db.execute("SELECT COUNT(*) FROM list_pc_classes;")[0].get("COUNT(*)")
+    # background_count = db.execute("SELECT COUNT(*) FROM list_backgrounds;")[0].get("COUNT(*)")
+    # spells_count = db.execute("SELECT COUNT(*) FROM list_spells;")[0].get("COUNT(*)")
+    # features_count = db.execute("SELECT COUNT(*) FROM list_pc_features;")[0].get("COUNT(*)")
+    # # okay so db.execute returns a list of dicts, so I grab the first element of that dict, and grab the value-pair of that dict
+    # # gods that is cursed, but it works lmao
+    # print("Race count is: ", race_count)
+    # #race_query = race_query.get("COUNT(*)")
+    # #var_count = 'COUNT(*)'
+    # #race_query = db.execute("SELECT ? FROM list_races", var_count)
+    # # this returns a dictionary, figure out how to properly interact with dictionaries and you should be good
+    # # a dictionary item on each line for each column I looked at
+    # # do yourself a favour and just zero-index all your CSVs, in the like AUTO-INCREMENT format and all that
+    # # that way, instead of going through all the items to check the primary key id,
+    # # I can just get a count of the number of items in the database
+    # race_list = []
+    # print("here:", race_query[0])
+    # #for var_line in race_query:
+        # #print(var_line)
+        # #for var_item in var_line:
+            # #print(var_item)
+        # #race_list.append(var_line[1])
+    # return race_query
     
 def main():
     race_list = valid_race_id(db)
