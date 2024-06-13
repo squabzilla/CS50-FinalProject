d1de31ba6d4c8ef43b7ab54965879dc8fdb21cf8
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-13 21:21:58 +0000

    Jun 13, 2024, 3:21 PM

--- Main/CustomClass_RPGcharacter.py
+++ Main/CustomClass_RPGcharacter.py
@@ -84,7 +84,9 @@ def class_spells_by_spell_level(class_column, spell_level):
     print("list_spells: ", list_spells)
     list_spell_ids = []
     for item in list_spells:
-        spell_id = list_spells[item].get("spell_id")
+        #spell_id = list_spells[item].get("spell_id")
+        spell = list_spells[item]
+        print(spell)
         #list_spell_ids.append(spell_id)
     return list_spell_ids
 
