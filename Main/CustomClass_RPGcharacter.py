a4ebe59e488d6265f9baaa8cc166834e68b6adf4
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-13 20:30:04 +0000

    Jun 13, 2024, 2:30 PM

--- Main/CustomClass_RPGcharacter.py
+++ Main/CustomClass_RPGcharacter.py
@@ -36,7 +36,7 @@ def highest_spell_slot(var_class_id, var_char_level):
     half_caster_IDs = [paladin_id, ranger_id]
     third_caster_IDs = []
     caster_level_multiplied = 0
-    max_spell = 0
+    max_spell_level = 0
     if var_class_id in full_caster_IDs: caster_level_multiplied = var_char_level * 3
     if var_class_id in half_caster_IDs: caster_level_multiplied = var_char_level * 2
     if var_class_id in third_caster_IDs: caster_level_multiplied = var_char_level * 1
@@ -44,17 +44,17 @@ def highest_spell_slot(var_class_id, var_char_level):
     # we just multiply everything by 3 - including the level threshholds for new spells
     count = 0
     while count <= caster_level_multiplied:
-        if count == ( 1 * 3): max_spell += 1    # note: 1 * 3 represents level 1, multiplied by 3
-        if count == ( 3 * 3): max_spell += 1
-        if count == ( 5 * 3): max_spell += 1
-        if count == ( 7 * 3): max_spell += 1
-        if count == ( 9 * 3): max_spell += 1
-        if count == (11 * 3): max_spell += 1
-        if count == (13 * 3): max_spell += 1
-        if count == (15 * 3): max_spell += 1
-        if count == (17 * 3): max_spell += 1
+        if count == ( 1 * 3): max_spell_level += 1    # note: 1 * 3 represents level 1, multiplied by 3
+        if count == ( 3 * 3): max_spell_level += 1
+        if count == ( 5 * 3): max_spell_level += 1
+        if count == ( 7 * 3): max_spell_level += 1
+        if count == ( 9 * 3): max_spell_level += 1
+        if count == (11 * 3): max_spell_level += 1
+        if count == (13 * 3): max_spell_level += 1
+        if count == (15 * 3): max_spell_level += 1
+        if count == (17 * 3): max_spell_level += 1
         count += 1
-    return True
+    return max_spell_level
 # TODO:
 # support for 1/3 casting-only-with-subclass can come with the level-up table
 # when I actually add that
