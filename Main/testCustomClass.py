65d3af5ba2240f480277229e67ec356faba417a6
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-14 21:30:04 +0000

    Jun 14, 2024, 3:30 PM

---
+++ Main/testCustomClass.py
@@ -0,0 +1,2 @@
+from CustomClass_RPGcharacter import highest_spell_slot, class_spells_by_spell_level, new_bard_spells, new_cleric_spells, \
+    new_druid_spells, new_ranger_spells, new_sorcerer_spells, new_warlock_spells, new_wizard_spells, rpg_char_create
