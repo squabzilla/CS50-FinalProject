8c0eed502deec855e0693c667e48a1fce8c66ce4
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-14 19:59:56 +0000

    Jun 14, 2024, 1:59 PM

--- Main/app.py
+++ Main/app.py
@@ -1,16 +1,17 @@
-#########################################################################################
-### below: copied imported libraries from: CS50 Week 9 C$50 Finance app.py (that was provided to us by CS50)
+# Import libraries
+# below: copied imported libraries from: CS50 Week 9 C$50 Finance app.py (that was provided to us by CS50)
 import os
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import login_required
+
+# above: copied imported libraries from: CS50 Week 9 C$50 Finance app.py (that was provided to us by CS50)
+import re # custom-built libraries I'm calling needs this, so I'm adding it just in case
 from CustomClass_RPGcharacter import highest_spell_slot, class_spells_by_spell_level, new_bard_spells, new_cleric_spells, \
     new_druid_spells, new_ranger_spells, new_sorcerer_spells, new_warlock_spells, new_wizard_spells, rpg_char_create
-### above: copied imported libraries from: CS50 Week 9 C$50 Finance app.py (that was provided to us by CS50)
-#########################################################################################
-
+# Note: some of these functions won't be called in this version, as functionality to create those classes is to be added later
 
 # configure flask application
 from flask import Flask
@@ -209,6 +210,8 @@ def create_character():
         if var_name != None:
             new_pc.name = var_name
         var_race_id = request.form.get("race_id")
+        var_class_id = request.form.get("class_id")
+        var_background_id = request.form.get("background_id")
         #character_name = request.form.get("character_name")
         #character_name = "Bob"
         #new_char.name = character_name
