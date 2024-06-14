50d0be2cc018371f4ca2ad76bab480d8fa03a9cd
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-14 18:12:24 +0000

    Jun 14, 2024, 12:12 PM

--- Main/test_py.py
+++ Main/test_py.py
@@ -8,10 +8,11 @@ def numberify(variable):
 def main():
     var = "soup"
     #print(numberify(var))
-    print("Test: comma-break, no space after breaksign, no space after colon:",var)
-    print("Test: comma-break, space after breaksign, no space after colon:", var)
-    print("Test: comma-break, space after breaksign, space after colon: ", var)
-    print("Test: plus-break, no space after breaksign, no space after colon:"+var)
-    print("Test: plus-break, space after breaksign, no space after colon:"+ var)
-    print("Test: plus-break, space after breaksign, space after colon: "+ var)
+    print("Test - desired soup column...                                colon: soup")
+    print("Test - comma-break, no space after breaksign, no space after colon:",var)
+    print("Test -    comma-break, space after breaksign, no space after colon:", var)
+    print("Test -       comma-break, space after breaksign, space after colon: ", var)
+    print("Test -  plus-break, no space after breaksign, no space after colon:"+var)
+    print("Test -     plus-break, space after breaksign, no space after colon:"+ var)
+    print("Test -        plus-break, space after breaksign, space after colon: " + var)
 main()
