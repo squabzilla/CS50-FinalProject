dd4ff3e64d1df5112f8872b4d9014c9e11f2bc70
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-15 02:07:41 +0000

    Jun 14, 2024, 8:07 PM

--- Main/app.py
+++ Main/app.py
@@ -227,14 +227,13 @@ def create_character():
                 #json_dump_2 = json.dumps(
         if var_race_id != None and new_pc.race_id == None:
             print("second if")
+            print("is var_race_id, of value:", var_race_id, ", considered numeric?", var_race_id.isnumeric)
             if var_race_id.isnumeric == True:
                 var_race_id = int(var_race_id)
                 if new_pc.set_race_id(var_race_id) == True: # remember my set_[attribute] functions will set the value AND return True or False depending on success
                     print("second-point-second if")
                     json_dump = json.dumps(db.execute("SELECT class_id, class_name FROM list_classes"))
                     print("new_pc.race_id =", new_pc.race_id)
-                else:
-                    print("we shouldn't be here")
         if var_class_id != None and new_pc.class_id == None:
             if var_class_id.isnumeric == True:
                 var_class_id = int(var_class_id)
