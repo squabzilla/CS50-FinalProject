24e8d55f289d3867f5463538c7b56aa7ee033dc2
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-19 20:30:52 +0000

    Jun 19, 2024, 2:30 PM

--- Main/import_text/testpathlib.py
+++ Main/import_text/testpathlib.py
@@ -1,4 +1,12 @@
 import os
 import pathlib
 
-print(os.getcwd())
+#print(os.getcwd())
+#os.path.dirname('C:\\')
+
+current_dir = os.getcwd()
+parent_dir = os.path.dirname(current_dir)
+print(parent_dir)
+
+main_dir = os.path.dirname(os.getcwd())
+print(main_dir)
