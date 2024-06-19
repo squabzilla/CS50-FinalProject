f33d808808a5e11cc53300ba35cc220eea792827
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-19 20:45:02 +0000

    Jun 19, 2024, 2:45 PM

--- Main/import_text/remove_extraneous_lines.py
+++ Main/import_text/remove_extraneous_lines.py
@@ -21,11 +21,13 @@ import shutil
 
 # folder-paths
 main_dir = os.path.dirname(os.getcwd())
+print(main_dir)
 var_input_text_path = "static/CSVs/TextFiles"
+print(var_input_text_path)
 var_input_text_path = pathlib.Path(var_input_text_path)
-print("var_input_text_path")
+print(var_input_text_path)
 var_input_text_path = os.path.join(main_dir, var_input_text_path)
-print("var_input_text_path")
+print(var_input_text_path)
 var_output_RegexModdedText_path = "static/CSVs/TextFiles_RegexAltered"
 var_output_RegexModdedText_path = pathlib.Path(var_output_RegexModdedText_path)
 var_output_RegexModdedText_path = os.path.join(main_dir, var_output_RegexModdedText_path)
@@ -56,7 +58,7 @@ var_csv_end = ".csv"
 
 # actual folder-paths
 var_input_text_path = pathlib.Path("D:\GitProjects\CS50-FinalProject\Main\static\CSVs\TextFiles")
-print("var_input_text_path")
+print(var_input_text_path)
 var_output_RegexModdedText_path = pathlib.Path("D:\GitProjects\CS50-FinalProject\Main\static\CSVs\RegexModded_TextFiles")
 
 # amount of files
