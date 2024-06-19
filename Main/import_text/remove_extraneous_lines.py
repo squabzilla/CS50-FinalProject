2bcb030a6b331d7e3d35c874579ce6852c287e7e
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-19 20:35:11 +0000

    Jun 19, 2024, 2:35 PM

--- Main/import_text/remove_extraneous_lines.py
+++ Main/import_text/remove_extraneous_lines.py
@@ -23,7 +23,7 @@ import shutil
 main_dir = os.path.dirname(os.getcwd())
 var_input_text_path = "static/CSVs/TextFiles"
 var_input_text_path = os.path.join(main_dir, var_input_text_path)
-var_output_RegexModdedText_path = "static/CSVs/TextFiles/TextFiles_RegexAltered"
+var_output_RegexModdedText_path = "static/CSVs/TextFiles_RegexAltered"
 var_output_RegexModdedText_path = os.path.join(main_dir, var_output_RegexModdedText_path)
 #var_input_text_path = pathlib.Path("D:\GitProjects\CS50-FinalProject\Main\static\CSVs\TextFiles")
 #var_output_RegexModdedText_path = pathlib.Path("D:\GitProjects\CS50-FinalProject\Main\static\CSVs\TextFiles_RegexAltered")
