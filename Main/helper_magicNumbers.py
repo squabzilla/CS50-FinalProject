5c85e3f8df659b5c0b64acc57e2efa06092a469f
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-23 04:38:20 +0000

    Jun 22, 2024, 10:38 PM

---
+++ Main/helper_magicNumbers.py
@@ -0,0 +1,17 @@
+class classIDs:
+    def __init__(self,
+                 Barbarian = 0, Bard = 1, Cleric = 2, Druid = 3,
+                 Fighter = 4, Monk = 5, Paladin = 6, Ranger = 7,
+                 Rogue = 8, Sorcerer = 9, Warlock = 10, Wizard = 11):
+        self.Barbarian = Barbarian
+        self.Bard = Bard
+        self.Cleric = Cleric
+        self.Druid = Druid
+        self.Fighter = Fighter
+        self.Monk = Monk
+        self.Paladin = Paladin
+        self.Ranger = Ranger
+        self.Rogue = Rogue
+        self.Sorcerer = Sorcerer
+        self.Warlock = Warlock
+        self.Wizard = Wizard
